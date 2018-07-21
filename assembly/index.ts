0a3cb996b49f6b142f72ec5acfd96589492b1d50
Jimmy Lai <laijimmy0@gmail.com>
2018-07-22 00:17:57 +0200

    project: rebooting ..

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,92 +1,29 @@
-import { Sphere } from './Sphere';
-import { Vector3 } from './Vector3';
-import { RayTracer } from './RayTracer';
-import 'allocator/arena';
+import 'allocator/tlsf';
+import { RayTracer, Pixel, getPixel, setPixel } from './RayTracer';
 declare namespace console {
   function debug(val: number): void;
 }
 
-export function init(width: i16, height: i16): void {
-  let scene: Array<Sphere> = new Array();
-  // push background sphere
-  scene.push(
-    new Sphere(
-      new Vector3(0.0, -10004, -20),
-      10000,
-      new Vector3(0.2, 0.2, 0.2),
-      0,
-      0,
-      new Vector3(),
-    ),
-  );
+export function init(width: i32 = 1000, height: i32 = 1000): void {
+  RayTracer.renderBufferWidth = width;
+  RayTracer.renderBufferLength = height;
+  RayTracer.renderBufferLength = width * height * sizeof<i32>();
+}
 
-  scene.push(
-    new Sphere(
-      new Vector3(0, 0, -20),
-      4,
-      new Vector3(1.0, 0.32, 0.36),
-      1,
-      0.5,
-      new Vector3(),
-    ),
-  );
-  scene.push(
-    new Sphere(
-      new Vector3(5, -1, -15),
-      2,
-      new Vector3(0.9, 0.76, 0.46),
-      1,
-      0,
-      new Vector3(),
-    ),
-  );
-  scene.push(
-    new Sphere(
-      new Vector3(5, 0, -25),
-      3,
-      new Vector3(0.65, 0.77, 0.97),
-      1,
-      0,
-      new Vector3(),
-    ),
-  );
-  scene.push(
-    new Sphere(
-      new Vector3(-5.5, 0, -15),
-      3,
-      new Vector3(0.9, 0.9, 0.9),
-      1,
-      0,
-      new Vector3(),
-    ),
-  );
+export function addSphere(): void {
+  let toto: Pixel = new Pixel();
+  toto.b = 50;
+  toto.r = 50;
+  toto.g = 50;
+  console.debug(changetype<usize>(toto));
+}
 
-  // push light
-  scene.push(
-    new Sphere(
-      new Vector3(0, 20, -30),
-      3,
-      new Vector3(),
-      0,
-      0,
-      new Vector3(1.2, 1.2, 1.2),
-    ),
-  );
-  scene.push(
-    new Sphere(
-      new Vector3(0, 10, 10),
-      3,
-      new Vector3(),
-      0,
-      0,
-      new Vector3(1, 1, 1),
-    ),
-  );
-
-  let backgroundColor: Vector3 = new Vector3(2.0, 2.0, 2.0);
-  console.debug(backgroundColor.x);
-  // create ray tracer
-  let rayTracer: RayTracer = new RayTracer(backgroundColor, scene);
-
-  rayTracer.render(width, height);
+export function test(): u8 {
+  setPixel(0, 0);
+  let toto: Pixel = getPixel(0, 0);
+  // toto.b = 255;
+  // toto.r = 255;
+  // toto.g = 255;
+  console.debug(changetype<usize>(toto));
+  return toto.r;
 }
